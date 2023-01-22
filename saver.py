d317fe61cd162a049d41c150dbdacdbf517b66c3
Alexander <job.nullptr@gmail.com>
2023-01-23 00:21:12 +0600

    add save_check_result

--- saver.py
+++ saver.py
@@ -2,4 +2,5 @@ import json
 
 
 def save_check_result(check_result: dict[int, dict[str, bool, bool, str]]) -> None:
-    ...
+    with open('urls.json', 'w') as f:
+        json.dump(check_result, f, ensure_ascii=False, indent=4)
