e716260180b176cc3a2bcd7bee4233efc821bfb4
Alexander <job.nullptr@gmail.com>
2023-01-28 17:28:06 +0600

    add url validation

--- onion_checker.py
+++ onion_checker.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import time
+import validators
 
-from url_validator import is_valid
 from availability_checker import is_available
 from saver import save_check_result
 
@@ -10,23 +10,22 @@ def check_onion(urls: list) -> None:
     check_result = dict()
 
     for index, url in enumerate(urls):
-        if url_is_valid := is_valid(url):
-            url_is_available = is_available(url)
+        url_is_valid: True | validators.ValidationFailure = validators.url(url)
 
+        if url_is_valid:
+            url_is_available = is_available(url)
         else:
-            url_is_valid = False
+            url_is_valid = False        # Cast validators.ValidationFailure to bool
             url_is_available = False
 
         t = time.time()
-        ts = datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
-
-        print(type(ts))
+        timestamp = datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
 
         check_result[index] = {
             "url": url,
             "is_valid": url_is_valid,
             "is_available": url_is_available,
-            "timestamp": ts
+            "timestamp": timestamp
         }
 
     save_check_result(check_result)
--- url_validator.py
+++
@@ -1,2 +0,0 @@
-def is_valid(url: str) -> bool:
-    return True     # TODO
