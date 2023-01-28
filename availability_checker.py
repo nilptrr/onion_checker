ef058a5a0927b25875e8f52170afd30186be1dc0
Alexander <job.nullptr@gmail.com>
2023-01-28 18:56:30 +0600

    add request timeout=5

--- availability_checker.py
+++ availability_checker.py
@@ -5,7 +5,7 @@ from config import proxies
 
 def is_available(url: str) -> bool:
     try:
-        requests.get(url, proxies=proxies)
+        requests.get(url, proxies=proxies, timeout=5)
 
     except requests.exceptions.RequestException:
         return False
